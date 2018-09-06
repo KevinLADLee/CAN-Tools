c5ff5b5dcddd93f4302008098eb5bc3d808c6ab2
KevinLADLee <kevinladlee@gmail.com>
2018-09-06 17:52:15 +0800

    Optimized receive function

--- can_device_linux.cpp
+++ can_device_linux.cpp
@@ -2,14 +2,51 @@
 
 #include "can_device_linux.h"
 
-CanDevice::CanDevice() {
- can_interface_ = "can0";
+CanDevice::CanDevice() : can_interface_("can0"),
+                         disable_filter_(false),
+                         loopback_(true),
+                         enable_timeout_(false){
+  timeout_config_.tv_sec = 0;
+  timeout_config_.tv_usec = 0;
 }
 
 CanDevice::CanDevice(std::string can_interface) :
-                     can_interface_(can_interface) {}
+                     can_interface_(can_interface),
+                     disable_filter_(false),
+                     loopback_(true) {}
+
+CanDevice::~CanDevice() {
+  close(sockfd_);
+}
 
 bool CanDevice::InitSocketCan() {
+
+  if(!Init()){
+    std::cout << "Init faild" << std::endl;
+    return false;
+  }
+
+  if(!Config()){
+    std::cout << "Configure failed" << std::endl;
+    return false;
+  }
+
+  return Bind();
+}
+
+
+bool CanDevice::InitSocketCanWithFilter(struct can_filter * filter, unsigned int num) {
+
+  if(!Init()){
+    return false;
+  }
+  setsockopt(sockfd_, SOL_CAN_RAW, CAN_RAW_FILTER,
+             filter, num * sizeof(struct can_filter));
+  Config();
+  return Bind();
+}
+
+bool CanDevice::Init() {
   sockfd_ = socket(PF_CAN, SOCK_RAW, CAN_RAW);
   if (sockfd_ < 0) {
     std::cout << "open error" << std::endl;
@@ -24,64 +61,76 @@ bool CanDevice::InitSocketCan() {
 
   can_addr_.can_family = AF_CAN;
   can_addr_.can_ifindex = ifreq_.ifr_ifindex;
-  rc_ = bind(sockfd_, reinterpret_cast<struct sockaddr *>(&can_addr_), sizeof(can_addr_));
-  if (rc_ == -1) {
-    std::cout << "bind failed" << std::endl;
+  if(!can_addr_.can_ifindex){
+    std::cout << "invalid bridge interface" << std::endl;
     return false;
   }
+
+  iov_.iov_base = &frame_;
+  msg_.msg_name = &can_addr_;
+  msg_.msg_iov = &iov_;
+  msg_.msg_iovlen = 1;
+  msg_.msg_control = &ctrlmsg_;
 
   return true;
 }
 
 
-bool CanDevice::InitSocketCanWithFilter(struct can_filter * filter) {
-  sockfd_ = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-  if (sockfd_ < 0) {
-    std::cout << "open error" << std::endl;
-    return false;
+bool CanDevice::Config(){
+  if(loopback_ == 0){
+    setsockopt(sockfd_, SOL_CAN_RAW, CAN_RAW_LOOPBACK, &loopback_, sizeof(loopback_));
   }
-
-  rc_ = setsockopt(sockfd_, SOL_CAN_RAW, CAN_RAW_FILTER,
-                   &filter, sizeof(can_filter));
-  if (rc_ == -1) {
-    std::cout << "setsockopt can_raw_filter failed" << std::endl;
-    return false;
+  if(disable_filter_){
+    setsockopt(sockfd_, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
   }
+  return true;
+}
 
-  std::strncpy(ifreq_.ifr_name, can_interface_.c_str(), IFNAMSIZ);
-  if (ioctl(sockfd_, SIOCGIFINDEX, &ifreq_) == -1) {
-    std::cout << "ioctl failed" << std::endl;
-    return false;
-  }
-
-  can_addr_.can_family = AF_CAN;
-  can_addr_.can_ifindex = ifreq_.ifr_ifindex;
+bool CanDevice::Bind() {
+  setsockopt(sockfd_, SOL_CAN_RAW, CAN_RAW_FD_FRAMES, &canfd_on, sizeof(canfd_on));
   rc_ = bind(sockfd_, reinterpret_cast<struct sockaddr *>(&can_addr_), sizeof(can_addr_));
-  if (rc_ == -1) {
+  if (rc_ < 0) {
     std::cout << "bind failed" << std::endl;
     return false;
   }
   return true;
 }
 
-int CanDevice::Read(uint8_t * buffer) {
-  struct can_frame frame;
-  read(sockfd_, &frame, CAN_MTU);
-  memcpy(buffer, frame.data, 8);
-  return frame.can_id;
-}
+int CanDevice::RecvMsg(struct canfd_frame * frame_ptr) {
+  FD_ZERO(&rdfs_);
+  FD_SET(sockfd_, &rdfs_);
 
+  if (FD_ISSET(sockfd_, &rdfs_)) {
+    int idx;
+    iov_.iov_len = sizeof(frame_);
+    msg_.msg_namelen = sizeof(can_addr_);
+    msg_.msg_controllen = sizeof(ctrlmsg_);
+    msg_.msg_flags = 0;
 
-int CanDevice::Read(unsigned int & can_id, uint8_t * buffer) {
-  struct can_frame frame;
-  read(sockfd_, &frame, CAN_MTU);
-  can_id = frame.can_id;
-  memcpy(buffer, frame.data, 8);
-  return 0;
-}
+    int nbyte = recvmsg(sockfd_, &msg_, 0);
 
-int CanDevice::Read(struct can_frame &frame) {
-  return read(sockfd_, &frame, CAN_MTU);
+    if(nbyte < 0){
+      if ((errno == ENETDOWN)) {
+        std::cerr << can_interface_ << ": interface down" << std::endl;
+        return 0;
+      }
+      return -1;
+    }
+
+    if((size_t)nbyte == CAN_MTU){
+      max_dlen_ = CAN_MAX_DLEN;
+    } else if((size_t)nbyte == CANFD_MTU){
+      max_dlen_ = CANFD_MAX_DLEN;
+    } else{
+      std::cerr << "read: incomplete CAN Frame" << std::endl;
+    }
+
+    *frame_ptr = frame_;
+    memcpy(frame_ptr->data, frame_.data, max_dlen_);
+    return max_dlen_;
+  }
+  return -1;
+
 }
 
 int CanDevice::Send(unsigned int can_id, const uint8_t *buffer) {
--- can_device_linux.h
+++ can_device_linux.h
@@ -5,7 +5,9 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+#include <memory>
 
+#include <stdint.h>
 #include <unistd.h>
 #include <linux/can.h>
 #include <linux/can/raw.h>
@@ -13,26 +15,34 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 
+constexpr int canfd_on = 1;
 
 class CanDevice{
+
  public:
   CanDevice();
 
   CanDevice(std::string can_interface);
 
+  ~CanDevice();
+
   bool InitSocketCan();
 
-  bool InitSocketCanWithFilter(struct can_filter * filter);
+  bool InitSocketCanWithFilter(struct can_filter * filter, unsigned int num);
 
-  int Read(uint8_t * buffer);
-
-  int Read(unsigned int & can_id, uint8_t * buffer);
-
-  int Read(struct can_frame & frame);
+  int RecvMsg(struct canfd_frame * frame_ptr);
 
   int Send(unsigned int can_id, const uint8_t *buffer);
 
   int Send(const struct can_frame & frame);
+
+ private:
+
+  bool Init();
+
+  bool Config();
+
+  bool Bind();
 
  private:
   std::string can_interface_;
@@ -40,6 +50,20 @@ class CanDevice{
   ifreq ifreq_;
   int sockfd_;
   int rc_;
+  int max_dlen_;
+
+  bool disable_filter_; // Used in applications only SEND CAN frames
+  bool loopback_; // Local loop back
+  bool enable_timeout_;
+  struct timeval timeout_config_;
+
+  fd_set rdfs_;
+  struct msghdr msg_;
+  struct cmsghdr *cmsg_;
+  struct canfd_frame frame_;
+  struct iovec iov_;
+  char ctrlmsg_[CMSG_SPACE(sizeof(struct timeval) + 3*sizeof(struct timespec) + sizeof(__u32))];
+
 
 };
 
